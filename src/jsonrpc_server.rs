0f1265f26d4f58e8d8ef46b6efd97ca6297d7fc0
code-pangolin <renchao@storswift.com>
2025-03-26 09:34:18 +0000

    feat: merge explorer rpc

--- src/jsonrpc_server.rs
+++ src/jsonrpc_server.rs
@@ -1,5 +1,5 @@
+use std::collections::HashMap;
 use std::net::SocketAddr;
-use std::str::FromStr;
 
 use axum::extract::{Path, State};
 use axum::{
@@ -7,11 +7,14 @@ use axum::{
     response::{IntoResponse, Response},
 };
 use axum_extra::response::ErasedJson;
-use tasm_lib::prelude::Digest;
+use block_selector::BlockSelectorExtended;
+use itertools::Itertools;
 use tokio::net::TcpListener;
 use tower_http::cors::CorsLayer;
 use tower_http::trace::TraceLayer;
 
+use crate::models::blockchain::block::block_info::BlockInfo;
+use crate::rpc_server::MempoolTransactionInfo;
 use crate::{
     models::blockchain::block::block_selector::BlockSelector, rpc_server::NeptuneRPCServer,
 };
@@ -52,17 +55,20 @@ pub(crate) async fn run_rpc_server(
     let router = {
         let routes = axum::Router::new()
             .route(
-                "/block/height/{height}",
-                axum::routing::get(get_block_by_height),
+                "/rpc/block/{*block_selector}",
+                axum::routing::get(get_block),
             )
             .route(
-                "/block/digest/{digest}",
-                axum::routing::get(get_block_by_digest),
+                "/rpc/block_info/{*block_selector}",
+                axum::routing::get(get_block_info),
             )
-            .route("/block/tip", axum::routing::get(get_tip))
             .route(
-                "/utxo_digest/{leaf_index}",
+                "/rpc/utxo_digest/{leaf_index}",
                 axum::routing::get(get_utxo_digest),
+            )
+            .route(
+                "/rpc/mempool/{start_index}/{number}",
+                axum::routing::get(get_mempool),
             );
 
         routes
@@ -83,9 +89,10 @@ pub(crate) async fn run_rpc_server(
 }
 
 async fn get_block(
-    rpcstate: NeptuneRPCServer,
-    block_selector: BlockSelector,
+    State(rpcstate): State<NeptuneRPCServer>,
+    Path(block_selector): Path<BlockSelectorExtended>,
 ) -> Result<ErasedJson, RestError> {
+    let block_selector = BlockSelector::from(block_selector);
     let state = rpcstate.state.lock_guard().await;
     let Some(digest) = block_selector.as_digest(&state).await else {
         return Err(RestError("block not found".to_owned()));
@@ -96,30 +103,6 @@ async fn get_block(
     };
 
     Ok(ErasedJson::pretty(block.block_with_invalid_proof()))
-}
-async fn get_block_by_height(
-    State(rpcstate): State<NeptuneRPCServer>,
-    Path(height): Path<u64>,
-) -> Result<ErasedJson, RestError> {
-    let block_selector = BlockSelector::Height(height.into());
-
-    get_block(rpcstate, block_selector).await
-}
-
-async fn get_block_by_digest(
-    State(rpcstate): State<NeptuneRPCServer>,
-    Path(digest): Path<String>,
-) -> Result<ErasedJson, RestError> {
-    let block_selector =
-        BlockSelector::Digest(Digest::from_str(&digest).map_err(|e| RestError(e.to_string()))?);
-
-    get_block(rpcstate, block_selector).await
-}
-
-async fn get_tip(State(rpcstate): State<NeptuneRPCServer>) -> Result<ErasedJson, RestError> {
-    let block_selector = BlockSelector::Tip;
-
-    get_block(rpcstate, block_selector).await
 }
 
 async fn get_utxo_digest(
@@ -135,4 +118,222 @@ async fn get_utxo_digest(
     };
 
     Ok(ErasedJson::pretty(digest))
+}
+async fn get_block_info(
+    State(rpcstate): State<NeptuneRPCServer>,
+    Path(block_selector): Path<BlockSelectorExtended>,
+) -> Result<ErasedJson, RestError> {
+    let block_selector = BlockSelector::from(block_selector);
+    let state = rpcstate.state.lock_guard().await;
+    let Some(digest) = block_selector.as_digest(&state).await else {
+        return Ok(ErasedJson::pretty(Option::<BlockInfo>::None));
+    };
+    let tip_digest = state.chain.light_state().hash();
+    let archival_state = state.chain.archival_state();
+
+    let Some(block) = archival_state.get_block(digest).await.unwrap() else {
+        return Ok(ErasedJson::pretty(Option::<BlockInfo>::None));
+    };
+    let is_canonical = archival_state
+        .block_belongs_to_canonical_chain(digest)
+        .await;
+
+    // sibling blocks are those at the same height, with different digest
+    let sibling_blocks = archival_state
+        .block_height_to_block_digests(block.header().height)
+        .await
+        .into_iter()
+        .filter(|d| *d != digest)
+        .collect();
+
+    let block_info = BlockInfo::new(
+        &block,
+        archival_state.genesis_block().hash(),
+        tip_digest,
+        sibling_blocks,
+        is_canonical,
+    );
+
+    Ok(ErasedJson::pretty(block_info))
+}
+
+async fn get_mempool(
+    State(rpcstate): State<NeptuneRPCServer>,
+    Path((start_index, number)): Path<(usize, usize)>,
+) -> Result<ErasedJson, RestError> {
+    let global_state = rpcstate.state.lock_guard().await;
+    let mempool_txkids = global_state
+        .mempool
+        .get_sorted_iter()
+        .skip(start_index)
+        .take(number)
+        .map(|(txkid, _)| txkid)
+        .collect_vec();
+
+    let (incoming, outgoing): (HashMap<_, _>, HashMap<_, _>) = {
+        let (incoming_iter, outgoing_iter) = global_state.wallet_state.mempool_balance_updates();
+        (incoming_iter.collect(), outgoing_iter.collect())
+    };
+
+    let tip_msah = global_state
+        .chain
+        .light_state()
+        .mutator_set_accumulator_after()
+        .hash();
+
+    let mempool_transactions = mempool_txkids
+        .iter()
+        .filter_map(|id| {
+            let mut mptxi = global_state
+                .mempool
+                .get(*id)
+                .map(|tx| (MempoolTransactionInfo::from(tx), tx.kernel.mutator_set_hash))
+                .map(|(mptxi, tx_msah)| {
+                    if tx_msah == tip_msah {
+                        mptxi.synced()
+                    } else {
+                        mptxi
+                    }
+                });
+            if mptxi.is_some() {
+                if let Some(pos_effect) = incoming.get(id) {
+                    mptxi = Some(mptxi.unwrap().with_positive_effect_on_balance(*pos_effect));
+                }
+                if let Some(neg_effect) = outgoing.get(id) {
+                    mptxi = Some(mptxi.unwrap().with_negative_effect_on_balance(*neg_effect));
+                }
+            }
+
+            mptxi
+        })
+        .collect_vec();
+
+    Ok(ErasedJson::pretty(mempool_transactions))
+}
+
+mod block_selector {
+    use std::str::FromStr;
+
+    use serde::de::Error;
+    use serde::{Deserialize, Deserializer};
+
+    use crate::models::blockchain::block::block_selector::{
+        BlockSelector, BlockSelectorParseError,
+    };
+
+    use height_or_digest::HeightOrDigest;
+
+    /// newtype for `BlockSelector` that provides ability to parse `height_or_digest/value`.
+    ///
+    /// This is useful for HTML form(s) that allow user to enter either height or
+    /// digest into the same text input field.
+    ///
+    /// In particular it is necessary to support javascript-free website with such
+    /// an html form.
+    #[derive(Debug, Clone, Copy)]
+    pub struct BlockSelectorExtended(BlockSelector);
+
+    impl std::fmt::Display for BlockSelectorExtended {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    impl FromStr for BlockSelectorExtended {
+        type Err = BlockSelectorParseError;
+
+        // note: this parses BlockSelector, plus height_or_digest/<value>
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            match BlockSelector::from_str(s) {
+                Ok(bs) => Ok(Self::from(bs)),
+                Err(e) => {
+                    let parts: Vec<_> = s.split('/').collect();
+                    if parts.len() == 2 && parts[0] == "height_or_digest" {
+                        Ok(Self::from(HeightOrDigest::from_str(parts[1])?))
+                    } else {
+                        Err(e)
+                    }
+                }
+            }
+        }
+    }
+
+    // note: axum uses serde Deserialize for Path elements.
+    impl<'de> Deserialize<'de> for BlockSelectorExtended {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let s = String::deserialize(deserializer)?;
+            Self::from_str(&s).map_err(D::Error::custom)
+        }
+    }
+
+    impl From<HeightOrDigest> for BlockSelectorExtended {
+        fn from(hd: HeightOrDigest) -> Self {
+            Self(hd.into())
+        }
+    }
+
+    impl From<BlockSelector> for BlockSelectorExtended {
+        fn from(v: BlockSelector) -> Self {
+            Self(v)
+        }
+    }
+
+    impl From<BlockSelectorExtended> for BlockSelector {
+        fn from(v: BlockSelectorExtended) -> Self {
+            v.0
+        }
+    }
+
+    mod height_or_digest {
+        use crate::models::blockchain::block::block_height::BlockHeight;
+        use crate::models::blockchain::block::block_selector::BlockSelector;
+        use crate::models::blockchain::block::block_selector::BlockSelectorParseError;
+        use crate::prelude::tasm_lib::prelude::Digest;
+        use serde::{Deserialize, Serialize};
+        use std::str::FromStr;
+
+        /// represents either a block-height or a block digest
+        #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+        pub enum HeightOrDigest {
+            /// Identifies block by Digest (hash)
+            Digest(Digest),
+            /// Identifies block by Height (count from genesis)
+            Height(BlockHeight),
+        }
+
+        impl std::fmt::Display for HeightOrDigest {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                match self {
+                    Self::Digest(d) => write!(f, "{}", d),
+                    Self::Height(h) => write!(f, "{}", h),
+                }
+            }
+        }
+
+        impl FromStr for HeightOrDigest {
+            type Err = BlockSelectorParseError;
+
+            // note: this parses the output of impl Display for HeightOrDigest
+            // note: this is used by clap parser in neptune-cli for block-info command
+            //       and probably future commands as well.
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                Ok(match s.parse::<u64>() {
+                    Ok(h) => Self::Height(h.into()),
+                    Err(_) => Self::Digest(Digest::try_from_hex(s)?),
+                })
+            }
+        }
+
+        impl From<HeightOrDigest> for BlockSelector {
+            fn from(hd: HeightOrDigest) -> Self {
+                match hd {
+                    HeightOrDigest::Height(h) => Self::Height(h),
+                    HeightOrDigest::Digest(d) => Self::Digest(d),
+                }
+            }
+        }
+    }
 }
